3bd8790893bf27c8a6506d1db4926d013590ca6d
AustinBoydston <austin.t.boydston-1@ou.edu>
2020-03-17 16:20:37 -0500

    final commit, working set

--- fizz-buzz.cpp
+++ fizz-buzz.cpp
@@ -1,44 +1,119 @@
 #include <iostream>
 #include <cstring>
+#include <sstream>
 using namespace std;
 
-void exit()
-{
-cout << "invalid" << endl;
-
-    return 0;
-}
-
-
-
+int exit();
 int main (int numArgs, char** argsv)
 {
+
+bool isNegative = false;
+
+
+
 if(numArgs != 2 )
 {
     cout << "Give me one parameter that is a number!" << endl;
     return 0;
 }
-for(int i = 0; i > strlen(argsv); i++)
-{
-    for(int j = 0; j > strlen(argsv[i]); j++; )
-    {
-switch(argsv[i][j])
-{
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
 
-    default : exit();
+    for(int j = 0; j < strlen(argsv[1]); j++ )
+    {
+       
+        if(j == 0)
+        {
+            switch(argsv[1][j])
+            {
+    case '-':  
+   isNegative = true;
+    break;   
+    case '0': break;
+    case '1': break;
+    case '2': break;
+    case '3': break;
+    case '4': break;
+    case '5': break;
+    case '6': break;
+    case '7': break;
+    case '8': break;
+    case '9': break;
+
+    default : return exit();
+                
+            }
+        }
+else
+{
+switch(argsv[1][j])
+{
+    
+    case '0': break;
+    case '1': break;
+    case '2': break;
+    case '3': break;
+    case '4': break;
+    case '5': break;
+    case '6': break;
+    case '7': break;
+    case '8': break;
+    case '9': break;
+
+    default : return exit();
 }
+}
+//loop ends
+}
+
+stringstream geek(argsv[1]);
+int value = 0;
+geek >> value;
+
+//count down or up to zero
+if(isNegative)
+{
+for(int i = value; i < 0; i++)
+    {
+cout << i;
+if(i % 3 == 0)
+{
+cout << " fizz";
+}
+if(i % 4 == 0)
+{
+cout << " buzz";
+}
+cout << endl;
+    }
+}
+else 
+{
+    for(int i = value; i > 0; i--)
+    {
+        cout << i;
+if(i % 3 == 0)
+{
+cout << " fizz";
+}
+if(i % 4 == 0)
+{
+cout << " buzz";
+}
+cout << endl;
+    
     }
 }
 
+    return 0;
+}
+
+
+
+
+
+////////////////////////////////////////////Method implementaion////////////////////////////////
+int exit()
+{
+cout << "You gave me one parameter, but it is not an integer!" << endl;
 
     return 0;
 }
